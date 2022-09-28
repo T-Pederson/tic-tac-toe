a5f00684d3327e1ca9a9e59f3ca51a5ac6b6fbf0
t <tysonpederson95@gmail.com>
2022-09-28 14:12:54 -0500

    Set up the basic styling

---
+++ tic-tac-toe.js
@@ -0,0 +1,5 @@
+// Store the gameboard as an array inside of a gameboard object (use module)
+
+// Store players as objects (use factory)
+
+// Create an object to control the flow of the game itself (use module)

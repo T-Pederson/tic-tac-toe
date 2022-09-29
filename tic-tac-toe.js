998eb8e56720ba1992d0803de6461c2b369d74ce
t <tysonpederson95@gmail.com>
2022-09-29 08:12:04 -0500

    Add event listeners to spaces, update populateGameboard function to update array based on what is being clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
 // Store the gameboard as an array inside of a gameboard object (use module)
 const gameboard = (() => {
-    let array = ["x", "", "o", "x", "x", "x", "o", "", "o"];
+    let array = ["", "", "", "", "", "", "", "", ""];
     return {array};
 })();
 
@@ -15,15 +15,31 @@ const player1 = player(1, 'x');
 const player2 = player(2, 'o');
 
 // Create an object to control the flow of the game itself (use module)
+const game = (() => {
+    // Display start game button
+    // Let player 1 pick their character
+    // Let player 2 pick their character
+    // Let player 1 have a turn
+    // Check for win, if no win let the next player go and so on
+    // Else if win end game
+    // Display winner
+    // Display reset button
+})();
 
 
 
-// Function to populate the gameboard from gameboard.array
-function populate () {
-    const markers = document.querySelectorAll(".marker");
+// Function to populate the gameboard based on player and what space was clicked
+function populateGameboard (space) {
+    // Need to fix this to detect current player rather than just x
+    space.currentTarget.innerText = "x";
+    const spaces = document.querySelectorAll(".space");
     for (let i = 0; i < gameboard.array.length; i++) {
-        markers[i].innerText = gameboard.array[i];
+        gameboard.array[i] = spaces[i].innerText;
     }
 }
 
-populate();
+
+// Add event listeners to let spaces be clicked
+document.querySelectorAll(".space").forEach(item => {
+    item.addEventListener("click", populateGameboard);
+})

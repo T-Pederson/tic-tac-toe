a0428f12b5cf887c32ad24b167922d6e2c172aa2
t <tysonpederson95@gmail.com>
2022-10-02 20:51:47 -0500

    Standardize apostrophe usage

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,10 @@
 // Set up global variables that all modules/factories need
-const spaces = document.querySelectorAll(".space");
+const spaces = document.querySelectorAll('.space');
 const statusText = document.querySelector('h2');
 
 // Store the gameboard as an array inside of a gameboard object (use module)
 const gameboard = (() => {
-    let array = ["", "", "", "", "", "", "", "", ""];
+    let array = ['', '', '', '', '', '', '', '', ''];
     const update = space => {
         // Generate an updated array of all current display spaces
         let spacesArray = [].slice.call(spaces, 0);
@@ -31,7 +31,7 @@ const gameboard = (() => {
         array[2] == 'x' && array[4] == 'x' && array[6] == 'x'
         ) {
             // remove event listeners that allow for turns and display x as winner
-            spaces.forEach(item => { item.removeEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.removeEventListener('click', game.currentPlayer.turn); });
             statusText.innerText = `${game.player1.getName()} wins!`;
             return true;
         } 
@@ -47,7 +47,7 @@ const gameboard = (() => {
         array[2] == 'o' && array[4] == 'o' && array[6] == 'o'
         ) { 
             // remove event listeners that allow for turns and display o as winner
-            spaces.forEach(item => { item.removeEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.removeEventListener('click', game.currentPlayer.turn); });
             statusText.innerText = `${game.player2.getName()} wins!`;
             return true;
         } 
@@ -64,7 +64,7 @@ const gameboard = (() => {
             array[8] != ''
         ) {
             // remove event listeners that allow for turns and display tie
-            spaces.forEach(item => { item.removeEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.removeEventListener('click', game.currentPlayer.turn); });
             statusText.innerText = "It's a tie!";
             return true;
         } else {
@@ -73,11 +73,11 @@ const gameboard = (() => {
     };
     // Erase all markers, reset array to all blank, reset status text to player 1's turn, change current player to player 1, and re-add all turn event listeners 
     const reset = () => {
-        spaces.forEach(item => { item.innerText = ""; });
-        array = ["", "", "", "", "", "", "", "", ""];
+        spaces.forEach(item => { item.innerText = ''; });
+        array = ['', '', '', '', '', '', '', '', ''];
         statusText.innerText = `${game.player1.getName()}'s turn...`;
         game.currentPlayer = game.player1;
-        spaces.forEach(item => { item.addEventListener("click", game.currentPlayer.turn); });
+        spaces.forEach(item => { item.addEventListener('click', game.currentPlayer.turn); });
     };
     return {update, winCheck, reset};
 })();
@@ -91,7 +91,7 @@ const player = (marker, name) => {
     const turn = space => {
         space = space.currentTarget;
         // Check to make sure the space hasn't already been played, then update the gameboard and check for a win, if no win change the current player
-        if (space.innerText == "") {
+        if (space.innerText == '') {
             gameboard.update(space);
             if (!(gameboard.winCheck())) {
                 game.changePlayer();
@@ -106,26 +106,26 @@ const player = (marker, name) => {
 // Create an object to control the flow of the game itself (use module)
 const game = (() => {
     // Set up players
-    let player1 = player("x", 'x');
-    let player2 = player("o", 'o');
+    let player1 = player('x', 'x');
+    let player2 = player('o', 'o');
     let currentPlayer = player1;
     statusText.innerText = `${currentPlayer.getName()}'s turn...`;
     // Set up event listeners on spaces to let players take turns
-    spaces.forEach(item => { item.addEventListener("click", currentPlayer.turn); });
+    spaces.forEach(item => { item.addEventListener('click', currentPlayer.turn); });
     // Set up event listener on reset button
-    document.querySelector("#reset").addEventListener("click", gameboard.reset);
-    // Update event listeners to reflect new current players, change current player, and change status text
+    document.querySelector('#reset').addEventListener('click', gameboard.reset);
+    // Update event listeners to reflect new currentPlayer, update currentPlayer, and update statusText
     const changePlayer = () => {
         if (game.currentPlayer.getMarker() == 'x') {
-            spaces.forEach(item => { item.removeEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.removeEventListener('click', game.currentPlayer.turn); });
             game.currentPlayer = player2;
             statusText.innerText = `${player2.getName()}'s turn...`;
-            spaces.forEach(item => { item.addEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.addEventListener('click', game.currentPlayer.turn); });
         } else {
-            spaces.forEach(item => { item.removeEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.removeEventListener('click', game.currentPlayer.turn); });
             game.currentPlayer = player1;
             statusText.innerText = `${player1.getName()}'s turn...`;
-            spaces.forEach(item => { item.addEventListener("click", game.currentPlayer.turn); });
+            spaces.forEach(item => { item.addEventListener('click', game.currentPlayer.turn); });
         }
     }
     return {player1, player2, currentPlayer, changePlayer}
